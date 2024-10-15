b7a0e85a658f8bae42b6546a6e4314d481763376
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-15 12:00:30 -0300

    Configurando o SaveButton

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  resources :cars, except: [:edit, :update]
-  resources :tracks, except: [:edit, :update]
-  resources :pitstop_strategies, only: [:new, :create]
-  
+  resources :cars, except: [:edit, :update]  # Rotas para carros
+  resources :tracks, except: [:edit, :update]  # Rotas para pistas
+
+  # Rotas para as corridas
   resources :races, except: [:edit, :update] do
     member do
-      get 'add_cars_tracks'
-      post 'create_cars_tracks'
+      post 'calculate_strategy'  # Para calcular a estratégia de pitstop para uma corrida específica
+    end
+
+    collection do
+      get 'add_cars_tracks'       # Para adicionar carros e pistas
+      post 'create_cars_tracks'   # Para criar carros e pistas (se necessário)
     end
   end
+  
+  # Rota para exibir uma corrida específica
+  get 'races/show/:car_id/:track_id', to: 'races#show', as: 'show_race'
 
-  root 'races#index'
+  # Rotas para estratégias de pitstop
+  resources :pitstop_strategies, only: [:new, :create]
+
+  root 'races#index'  # Define a rota raiz para a lista de corridas
 end
